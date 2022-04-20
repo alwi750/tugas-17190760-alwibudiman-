ed83264476a466b17d02bf7de5c3ae4de6da4545
alwi750 <alwibudiman05@gmail.com>
2022-04-20 11:37:04 +0700

    pustaka booking

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : alwi</li>
+ <li>Nama Belakang : budiman</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan Cilenggang Raya No. 165 cilenggang</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Cianjur</li>
+ </ul>
+ <h4>Hobi</h4>
+ <ul type="square">
+ <li>Main Game</li>
+ <li>Baca Komik</li>
+ </ul>
+</section>
